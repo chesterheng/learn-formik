2ba9d2a5ca65fd40a157d7d31cee85c4c1536349
Chester Heng <chesterheng@Chesters-MacBook-Pro.local>
2019-09-28 19:56:52 +0800

    03:validate form with formik and yup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import "./App.css";
 import React, { Fragment, useState } from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import ItemList from "./ItemList";
 
 const App = () => {
   const [items, setItems] = useState([]);
 
-  const handleSubmit = ({ item }) => {
-    setItems(prevState => [...prevState, item]);
+  const initialValues = {
+    item: ""
   };
+
+  const handleSubmit = ({ item }, { resetForm }) => {
+    setItems(prevState => [...prevState, item]);
+    resetForm(initialValues);
+  };
+
+  const validationSchema = Yup.object().shape({
+    item: Yup.string().required("Item name is required")
+  });
 
   return (
     <Fragment>
       <h2>To do List:</h2>
       <ItemList items={items} />
-      <Formik onSubmit={handleSubmit}>
+      <Formik
+        onSubmit={handleSubmit}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+      >
         <Form>
           <label htmlFor="name">Item:</label>
           <Field type="text" name="item" />
+          <ErrorMessage name="item" />
           <button type="submit">Add Item</button>
         </Form>
       </Formik>
